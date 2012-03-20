4b6b502d694bfe419c9a0140b96dcec4c96a87f1
Stef Walter <stefw@gnome.org>
2012-03-20 10:04:57 +0100

    Initial commit

---
+++ service/ic-main.c
@@ -0,0 +1,20 @@
+/* identity-config - Identity configuration service
+ *
+ * Copyright 2012 Red Hat Inc
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; either version 2 of the licence or (at
+ * your option) any later version.
+ *
+ * See the included COPYING file for more information.
+ */
+
+#include "config.h"
+
+int
+main (int argc,
+      char *argv[])
+{
+	return 0;
+}
